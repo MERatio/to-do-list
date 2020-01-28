2ffec6768cf6d10a55c6d7254fdc2abb74333eb0
Mark Edward Ratio <markedwardratio@gmail.com>
2020-01-28 16:33:21 +0800

    Updates

---
+++ precache-manifest.6c468c6a1e9a1b439c55e56957eb1477.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54a47676f318aa4aa76494ad42d816fc",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "3e18c198596023ba4f36",
+    "url": "/to-do-list/static/css/main.e4ea7383.chunk.css"
+  },
+  {
+    "revision": "3bb4e06a52be089d163c",
+    "url": "/to-do-list/static/js/2.fd384683.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/to-do-list/static/js/2.fd384683.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3e18c198596023ba4f36",
+    "url": "/to-do-list/static/js/main.af9de47e.chunk.js"
+  },
+  {
+    "revision": "ef1ab25b4db95c178911",
+    "url": "/to-do-list/static/js/runtime-main.7557c059.js"
+  }
+]);
